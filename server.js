68240604f5a43b2bee9e11f01f9a9295ffea961f
Edgar Vargas <vargased23@gmail.com>
2023-02-05 22:52:46 -0800

    added server.js and code for server.js

---
+++ server.js
@@ -0,0 +1,26 @@
+
+const PORT = process.env.PORT || 3001;
+const express = require('express');
+const app = express();
+const fs = require('fs');
+const path = require('path');
+
+// helper methods
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+// middleware for URL encoded data
+app.use(express.urlencoded({
+  extended: true
+}));
+
+app.use(express.static('public'));
+
+// middleware for the parsing of JSON data
+app.use(express.json());
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+app.listen(PORT, () => {
+  console.log(`API server is now on port ${PORT}!`);
+});
